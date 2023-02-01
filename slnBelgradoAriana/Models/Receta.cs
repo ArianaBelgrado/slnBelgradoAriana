243555b7c9bd140f11ac0d639da158e551a122b6
Ariana Belgrado <belgradoariana@gmail.com>
2023-02-01 16:08:07 -0300

    .

--- slnBelgradoAriana/Models/Receta.cs
+++ slnBelgradoAriana/Models/Receta.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace slnBelgradoAriana.Models
 {
+    [Table("Receta")]
     public class Receta
     {
         public int RecetaId { get; set; }
